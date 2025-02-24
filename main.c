9d65e8383cf5f6dfdbcbf2bc3429bb34a5495910
Puttipong Chutipongwanit <puttipong.chu@student.mahidol.edu>
2025-02-24 09:17:18 +0000

    print format

--- main.c
+++ main.c
@@ -16,14 +16,14 @@ for ( i = 1; i < argc; ++i ) {
    // traverse the tree preOrder
    printf("\nThe preOrder traversal is:\n" );
    preOrder( b.root );
+   
+   // traverse the tree postOrder
+   printf("\nThe postOrder traversal is:\n" );   
+   postOrder( b.root );
 
    // traverse the tree inOrder
    printf("\nThe inOrder traversal is:\n" );
    inOrder( b.root );
-
-   // traverse the tree postOrder
-   printf("\nThe postOrder traversal is:\n" );   
-   postOrder( b.root );
 
    printf("\n");
    printTree( b.root, 0 );
